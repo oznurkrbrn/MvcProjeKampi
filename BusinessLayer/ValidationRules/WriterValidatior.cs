b6c08185b01e98ca33e9001144367f456bb2301f
Öznur Karaburun <oznurr03@hotmail.com>
2021-11-11 12:33:50 +0300

    Writer Update

---
+++ BusinessLayer/ValidationRules/WriterValidatior.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidatior:AbstractValidator<Writer>
+    {
+        public WriterValidatior()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadını boş geçemezsiniz");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Yazar unvanını boş geçemezsiniz");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Yazar soyadını boş geçemezsiniz");
+            RuleFor(x => x.WriterSurname).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapın.");
+            RuleFor(x => x.WriterSurname).MaximumLength(20).WithMessage("Lütfen 20  karakterden fazla değer girişi yapmayın.");
+            RuleFor(x => x.WriterAbout).Must(x => x != null && x.ToUpper().Contains("A")).WithMessage("Hakkında kısmında en az bir a harfi içermelidir");
+        }
+    }
+}
